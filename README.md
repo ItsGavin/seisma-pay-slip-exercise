1bf92bcfe23d44401196d061c6462dfbe62dddce
Gavin <gavin_ng96@hotmail.com>
2021-08-02 13:55:02 +1000

    Set up "resources" folder Main.java now reads csv files from "resources"

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # seisma-pay-slip-exercise
 Seisma recruitment exercise: Monthly employee pay slip calculation program
+
+# Assumptions
+The input and output CSV files are assumed to have no headings and contain raw data from line 1 onwards
+CSV line delimiter is a comma(,)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    static String INPUTDATA = "resources/input.csv";
     public static void main(String[] args) throws Exception {
+        List<List<String>> employee_details = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader(INPUTDATA))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] values = line.split(",");
+                employee_details.add(Arrays.asList(values));
+            }
+        }
+        
+        System.out.println(employee_details);
         System.out.println("Hello, World!");
     }
 }
