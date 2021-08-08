7b8fc2ef29cf3f9665c995a8b9a825239872cd23
Gavin <gavin_ng96@hotmail.com>
2021-08-09 03:10:37 +1000

    Initiliased Employee.java unit test class

---
+++ src/test/java/model/EmployeeTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for Employee class.
+ */
+public class EmployeeTest {
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
